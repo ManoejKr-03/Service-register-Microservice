e8981a8216c063c48acc51a4ede7640efc9c8b11
manoejKr <287642@ust.com>
2024-10-06 21:42:22 +0530

    commaned in main class

--- src/main/java/com/demo/service_register/ServiceRegisterApplication.java
+++ src/main/java/com/demo/service_register/ServiceRegisterApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+// it is the service-register in spring boot
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegisterApplication {
